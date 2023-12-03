00ecdff7e602057c4b807dd636e0091974478436
aidashhdn <152787925+aidashhdn@users.noreply.github.com>
2023-12-04 00:04:47 +0800

    Add files via upload

---
+++ rm1.js
@@ -0,0 +1,7 @@
+// Function to handle scrolling to a specific section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
